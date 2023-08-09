52d2e7012ecadee2c264ced5c7d027ca95be853f
miracle 2022 <funmilayoakinwale@Prospas-MacBook-Pro.local>
2023-08-10 00:21:51 +0100

    updated yaml file

--- cypress/e2e/pokeapi.cy.js
+++ cypress/e2e/pokeapi.cy.js
@@ -2,4 +2,4 @@ it("GET API testing Using Cypress API Plugin", () => {
   cy.api("GET", "https://pokeapi.co/api/v2/pokemon/ditto").should((response) => {
     expect(response.status).to.eq(200);
 });
-});
+})
