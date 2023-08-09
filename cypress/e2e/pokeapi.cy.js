d0c5cc5116d3b637c4c29d90207a28e97aaf74c3
miracle 2022 <funmilayoakinwale@Prospas-MacBook-Pro.local>
2023-08-10 00:15:20 +0100

    poke-api-push

---
+++ cypress/e2e/pokeapi.cy.js
@@ -0,0 +1,5 @@
+it("GET API testing Using Cypress API Plugin", () => {
+  cy.api("GET", "https://pokeapi.co/api/v2/pokemon/ditto").should((response) => {
+    expect(response.status).to.eq(200);
+});
+});
